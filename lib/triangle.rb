6b7c7c7c1a3e8df2fbe2da89f93abb8f11e6ba65
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-04-07 15:46:41 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,6 +9,9 @@ class Triangle
   end
 
   def kind
+    lengths = [@length_1, @length_2, @length_3]
+    if @length_1 == 0 || @length_2 == 0 || @length_3 == 0 || 
+      
     if @length_1 == @length_2 && @length_2 == @length_3
       :equilateral
     elsif @length_1 == @length_2 && @length_2 != @length_3
