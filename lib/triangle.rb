128b9ef12a06fdc7bd96970929565797d7d59da8
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-04-07 15:43:35 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -5,8 +5,11 @@ class Triangle
   def initialize(length_1, length_2, length_3)
     @length_1 = length_1
     @length_2 = length_2
-    @length_3 = length_3 
+    @length_3 = length_3
   end
+
+  def kind
+    if @length_1 == @length_2 && @length_2 == @length_3 
 
 
 
