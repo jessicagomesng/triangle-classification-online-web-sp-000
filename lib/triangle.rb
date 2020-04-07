f7566dc0dc25a867350218e38f96fa32aa5221de
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-04-07 15:58:35 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,7 @@ class Triangle
 
   def kind
     lengths = [@length_1, @length_2, @length_3]
-    sum_of_lengths = [@length_1 + @length_2, @length_2 + @length_3, @length_3 + @length_1]
+    sum_1 = @length_1 + @length_2, @length_2 + @length_3, @length_3 + @length_1]
 
     if lengths.any? { |length| length <= 0 }
       raise TriangleError
