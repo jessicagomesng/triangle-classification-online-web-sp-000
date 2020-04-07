b024c2a4ded6bcb315e1c135013c964741837556
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-04-07 15:49:33 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,7 +15,6 @@ class Triangle
       raise TriangleError
     elsif @length_1 + @length_2 < @length_3 || @length_2 + @length_3 < @length_1 || @length_1 + @length_3 < @length_2
       raise TriangleError 
-
     elsif @length_1 == @length_2 && @length_2 == @length_3
       :equilateral
     elsif @length_1 == @length_2 && @length_2 != @length_3
