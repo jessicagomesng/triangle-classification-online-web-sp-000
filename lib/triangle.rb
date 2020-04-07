6c6105aa37896293609f4c4b354e31b336d22a1b
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-04-07 15:46:45 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,8 +10,8 @@ class Triangle
 
   def kind
     lengths = [@length_1, @length_2, @length_3]
-    if @length_1 == 0 || @length_2 == 0 || @length_3 == 0 || 
-      
+    if @length_1 == 0 || @length_2 == 0 || @length_3 == 0 ||
+
     if @length_1 == @length_2 && @length_2 == @length_3
       :equilateral
     elsif @length_1 == @length_2 && @length_2 != @length_3
@@ -22,7 +22,7 @@ class Triangle
   end
 
   class TriangleError < StandardError
-  end 
+  end
 
 
 end
